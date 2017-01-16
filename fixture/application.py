0786995ec72ab3e880baab097201b91055efda3c
talareq <galuszka.grzegorz@yahoo.com>
2017-01-16 21:09:50 +0000

    working on emails

--- fixture/application.py
+++ fixture/application.py
@@ -5,7 +5,7 @@ from fixture.project import ProjectHelper
 from fixture.james import JamesHelper
 from fixture.signup import SignupHelper
 from fixture.mail import MailHelper
-
+from fixture.soap import SoapHelper
 
 
 class Application:
@@ -23,6 +23,7 @@ class Application:
         self.project = ProjectHelper(self)
         self.james = JamesHelper(self)
         self.mail = MailHelper(self)
+        self.soap = SoapHelper
         self.signup = SignupHelper(self)
         self.config = config
         self.base_url = config['web']['baseUrl']
---
+++ fixture/soap.py
@@ -0,0 +1,25 @@
+from suds.client import Client
+from suds import WebFault
+from model.formfiller import Project
+
+class SoapHelper:
+
+    def __init__(self, app):
+        self.app = app
+
+    def can_login(self, username, password):
+        client = Client("http://localhost/mantisbt-1.2.19/api/soap/mantisconnect.php?wsdl")
+        try:
+            client.service.mc_login(username, password)
+            return True
+        except WebFault:
+            return False
+
+    project_cache = None
+
+    def get_project_list(self):
+        if self.project_cache is None:
+            client = Client("http://localhost/mantisbt-1.2.19/api/soap/mantisconnect.php?wsdl")
+            client.service.mc_project_get_all_subprojects
+            self.project_cache.append(Project(id=id, name=name, description=description))
+        return list(self.project_cache)
