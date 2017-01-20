34219926081cf379bee3e53a2a93ee8d1e6d5bc6
talareq <galuszka.grzegorz@yahoo.com>
2017-01-20 15:44:30 +0000

    Homework 25,26
    Add_new_project
    Delete_project
    
    Loading project list via SOAP MC

--- generator/project.py
+++ generator/project.py
@@ -29,9 +29,9 @@ def random_string(prefix, maxlen):
 
 
 
-testdata = [Project(name="", description="")] + [
+testdata =[
     Project(name=random_string("name", 10), description=random_string("description", 20))
-    for i in range(5)
+    for i in range(1)
 ]
 
 
--- test/delete_project.py
+++ test/delete_project.py
@@ -4,13 +4,15 @@ import random
 def test_del_project(app):
     app.session.login("administrator", "root")
     assert app.session.is_logged_in_as("administrator")
-    old_projects=app.project.get_project_list()
+    username="administrator"
+    password="root"
+    old_projects=app.soap.get_project_list(username, password)
     if len(old_projects) == 0:
         app.project.new_project(Project(name="test", description="test"))
-        old_projects=app.project.get_project_list()
+        old_projects=app.soap.get_project_list(username, password)
     return old_projects
     Project = random.choice(old_projects)
     app.project.del_project(Project.id)
-    new_projects=app.project.get_project_list()
+    new_projects=app.soap.get_project_list(username, password)
     old_projects.remove(Project)
     assert old_projects == new_projects
